7eebf100d2e21257344638c7dc1a1edf38a6d678
Dan Saattrup Smart <dan.smart@alexandra.dk>
2026-05-02 20:32:11 +0200

    feat: reorder _split_text below chunk_transcriptions and use kwargs

--- src/but_with_subs/text_chunking.py
+++ src/but_with_subs/text_chunking.py
@@ -12,7 +12,55 @@ from .logging_config import logger
 nltk.download("punkt", quiet=True)
 
 
-def _split_text(text: str, max_words: int) -> list[str]:
+def chunk_transcriptions(
+    transcriptions: list[Transcription], max_words: int
+) -> list[Transcription]:
+    """Split transcriptions into segments.
+
+    Args:
+        transcriptions:
+            A list of Transcription objects to be chunked.
+        max_words:
+            The maximum number of words per segment.
+
+    Returns:
+        A list of Transcription objects, each containing a segment of the original
+        transcription.
+    """
+    text = " ".join([transcription.text for transcription in transcriptions])
+    text = PunctFixer(language="da").punctuate(text=text)
+
+    segments = _split_text(text=text, max_words=max_words)
+
+    for segment in segments:
+        first_word = segment.split(" ")[0].lower().strip(string.punctuation)
+        first_word_candidates = [
+            transcription
+            for transcription in transcriptions
+            if transcription.text == first_word
+        ]
+        if not first_word_candidates:
+            logger.warning(
+                f"Could not find transcription for {first_word!r}. Skipping."
+            )
+            continue
+        first_word_time = first_word_candidates[0].start_time
+
+        last_word = segment.split(" ")[-1].lower().strip(string.punctuation)
+        last_word_candidates = [
+            transcription
+            for transcription in transcriptions
+            if transcription.text == last_word
+        ]
+        if not last_word_candidates:
+            logger.warning(f"Could not find transcription for {last_word!r}. Skipping.")
+            continue
+        last_word_time = last_word_candidates[0].end_time
+
+        Transcription(start_time=first_word_time, end_time=last_word_time, text=segment)
+
+
+def _split_text(*, text: str, max_words: int) -> list[str]:
     """Split text into smaller segments if they exceed max_words.
 
     Args:
@@ -73,51 +121,3 @@ def _split_text(text: str, max_words: int) -> list[str]:
         " ".join(words[i : i + max_words])
         for i in range(0, len(words), max_words)
     ]
-
-
-def chunk_transcriptions(
-    transcriptions: list[Transcription], max_words: int
-) -> list[Transcription]:
-    """Split transcriptions into segments.
-
-    Args:
-        transcriptions:
-            A list of Transcription objects to be chunked.
-        max_words:
-            The maximum number of words per segment.
-
-    Returns:
-        A list of Transcription objects, each containing a segment of the original
-        transcription.
-    """
-    text = " ".join([transcription.text for transcription in transcriptions])
-    text = PunctFixer(language="da").punctuate(text=text)
-
-    segments = _split_text(text, max_words)
-
-    for segment in segments:
-        first_word = segment.split(" ")[0].lower().strip(string.punctuation)
-        first_word_candidates = [
-            transcription
-            for transcription in transcriptions
-            if transcription.text == first_word
-        ]
-        if not first_word_candidates:
-            logger.warning(
-                f"Could not find transcription for {first_word!r}. Skipping."
-            )
-            continue
-        first_word_time = first_word_candidates[0].start_time
-
-        last_word = segment.split(" ")[-1].lower().strip(string.punctuation)
-        last_word_candidates = [
-            transcription
-            for transcription in transcriptions
-            if transcription.text == last_word
-        ]
-        if not last_word_candidates:
-            logger.warning(f"Could not find transcription for {last_word!r}. Skipping.")
-            continue
-        last_word_time = last_word_candidates[0].end_time
-
-        Transcription(start_time=first_word_time, end_time=last_word_time, text=segment)
