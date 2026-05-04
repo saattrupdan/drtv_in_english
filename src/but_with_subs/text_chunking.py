294ba7d41d619ab0b31eaf362469a635c94d14aa
Dan Saattrup Smart <dan.smart@alexandra.dk>
2026-05-04 15:03:46 +0200

    fix: punkt_tab > punkt

--- src/but_with_subs/text_chunking.py
+++ src/but_with_subs/text_chunking.py
@@ -11,7 +11,6 @@ from .constants import DA, MIN_CHUNK_LENGTH_SECONDS
 from .data_models import Chunk
 from .logging_config import logger
 
-nltk.download("punkt", quiet=True)
 nltk.download("punkt_tab", quiet=True)
 
 PUNCTUATION_PATTERN = rf"[{string.punctuation}]"
