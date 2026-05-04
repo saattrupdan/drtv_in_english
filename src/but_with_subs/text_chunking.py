4e7a84cf2eb33a62137cf45e5744e13d58642ed6
Dan Saattrup Smart <dan.smart@alexandra.dk>
2026-05-04 15:09:56 +0200

    chore: Set quiet=True

--- src/but_with_subs/text_chunking.py
+++ src/but_with_subs/text_chunking.py
@@ -11,7 +11,7 @@ from .constants import DA, MIN_CHUNK_LENGTH_SECONDS
 from .data_models import Chunk
 from .logging_config import logger
 
-nltk.download("punkt_tab")
+nltk.download("punkt_tab", quiet=True)
 
 PUNCTUATION_PATTERN = rf"[{string.punctuation}]"
 
